c1e3e8404d9146d6e5590c0998b911f91d3c5ce9
niteshprajapati2932 <74131377+niteshprajapati2932@users.noreply.github.com>
2020-11-12 20:18:26 +0530

    Update auto.js

--- commands/auto.js
+++ commands/auto.js
@@ -9,21 +9,59 @@
   aliases: 
 CMD*/
 
+let lib = Libs.ReferralLib
+
+var refList = lib.currentUser.refList.get();
+
 var withdrawn = Bot.getProperty("totalWithdrawn");
+
 withdrawn = parseFloat(withdrawn); 
+
 var wallet = User.getProperty("TRXwallet");
+
 var balance = Libs.ResourcesLib.userRes("balance")
-if(data.message < 2500){
+
+if(message < 2500){
+
 Bot.sendMessage("_❌ Minimum withdraw 2500 Santa_")
+
 }else{
-if(data.message > balance.value()){
+
+if(message > balance.value()){
+
 Bot.sendMessage("_❌ Maximum withdraw "+balance.value().toFixed(6)+" Santa_")
+
 }else{
-Bot.sendMessage("*✅ Withdrawal sent\n\n💳 Transaction Details :\n 💰Amount : "+data.message+" Santa\n💼 wallet : "+wallet+"\n\n⛔️ May be it will take upto 24 hours*")
-balance.add(-data.message)
+
+Bot.sendMessage("*✅ Withdrawal sent\n\n💳 Transaction Details :\n 💰Amount : "+message+" Santa\n💼 wallet : "+wallet+"\n\n⛔️ May be it will take upto 24 hours*")
+
+balance.add(-message)
+
 Api.sendMessage({ 
+
 chat_id: "@SantaBotPayment", 
-text: "*🔋 New Approved Payout\n\n▪️ Status : Confirmed\n▪️ User Id :* "+user.telegramid+"\n\n ▪️ User username - @"+user.username+"\n\n*▪️ Amount :* "+data.message+" Santa\n\n*♦️ Paid Address ♦️\n "+wallet+"\n\n✅ Bot @SantaTokenAirdropBot*" , 
+
+text: "*🔋 New Approved Payout\n\n▪️ Status : Confirmed\n▪️ User Id :* "+user.telegramid+"\n\n ▪️ User username - @"+user.username+"\n\n▪️Number of referrals : "+refList.length+"\n\n*▪️ Amount :* "+message+" Santa\n\n*♦️ Paid Address ♦️\n "+wallet+"\n\n✅ Bot @SantaTokenAirdropBot*" , 
+
 parse_mode: "Markdown"})
+
 }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
